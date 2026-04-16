3adf6ac9d2aa91fff59d5c614869119ac5aae5a6
acassaigne <anthony.cassaigne@gmail.com>
2026-04-16 17:56:45 +0200

    todo: rewrite indexOfAny

--- first.clj
+++ first.clj
@@ -180,3 +180,23 @@
   (if (zero? x)
     result
     (recur (conj result x) (dec x)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Where's My for Loop? ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;     public static int indexOfAny(String str, char[] searchChars) {
+;;         if (isEmpty(str) || ArrayUtils.isEmpty(searchChars)) {
+;;             return -1;
+;;         }
+;;         for (int i = 0; i < str.length(); i++) {
+;;             char ch = str.charAt(i);
+;;             for (int j = 0; j < searchChars.length; j++) {
+;;                 if (searchChars[j] == ch) {
+;;                     return i;
+;;                 }
+;;             }
+;;         }
+;;         return -1;
+;;     }
+;; rewrite it in clojure
